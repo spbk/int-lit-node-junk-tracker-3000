34ddc47c66f8bbd63bf227095844a9fc3e2d1899
Chris Baclig <chris@baclig.me>
2021-10-06 20:57:52 +0000

    Removing React templating and added example Lit element instead

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Import any Lit elements here
+import './components/my-element.js';
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
